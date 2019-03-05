d9322b2308846c1f66a878db3c1c4776f11b7094
winsider <lars.tungen@abaris.no>
2019-03-05 11:45:21 +0100

    Generated from SAFE template with Azure publishing

---
+++ src/Server/Server.fs
@@ -0,0 +1,48 @@
+open System.IO
+open System.Threading.Tasks
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open FSharp.Control.Tasks.V2
+open Giraffe
+open Saturn
+open Shared
+
+open Microsoft.WindowsAzure.Storage
+
+let tryGetEnv = System.Environment.GetEnvironmentVariable >> function null | "" -> None | x -> Some x
+
+let publicPath = tryGetEnv "public_path" |> Option.defaultValue "../Client/public" |> Path.GetFullPath
+let storageAccount = tryGetEnv "STORAGE_CONNECTIONSTRING" |> Option.defaultValue "UseDevelopmentStorage=true" |> CloudStorageAccount.Parse
+
+let port = "SERVER_PORT" |> tryGetEnv |> Option.map uint16 |> Option.defaultValue 8085us
+
+let getInitCounter() : Task<Counter> = task { return { Value = 42 } }
+
+let webApp = router {
+    get "/api/init" (fun next ctx ->
+        task {
+            let! counter = getInitCounter()
+            return! json counter next ctx
+        })
+}
+
+let configureSerialization (services:IServiceCollection) =
+    services.AddSingleton<Giraffe.Serialization.Json.IJsonSerializer>(Thoth.Json.Giraffe.ThothSerializer())
+
+let configureAzure (services:IServiceCollection) =
+    tryGetEnv "APPINSIGHTS_INSTRUMENTATIONKEY"
+    |> Option.map services.AddApplicationInsightsTelemetry
+    |> Option.defaultValue services
+
+let app = application {
+    url ("http://0.0.0.0:" + port.ToString() + "/")
+    use_router webApp
+    memory_cache
+    use_static publicPath
+    service_config configureSerialization
+    service_config configureAzure
+    use_gzip
+}
+
+run app
